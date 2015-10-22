891fdbd39dc9e8a54a3c27fa7f95cb19a4fe4c42
unknown <a01911@ELS-H65580.LA.FRD.DIRECTV.com>
2015-10-22 15:39:05 -0700

    Added the address for restaurant batch

--- src/main/java/com/tagdish/dao/jdbc/rowmapper/AccountRowMapper.java
+++ src/main/java/com/tagdish/dao/jdbc/rowmapper/AccountRowMapper.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.tagdish.domain.db.AccountDB;
+import com.tagdish.domain.db.GeoTargetDB;
+import com.tagdish.domain.location.Location;
 
 public class AccountRowMapper implements RowMapper<AccountDB>{
 
@@ -13,7 +15,7 @@ public class AccountRowMapper implements RowMapper<AccountDB>{
 		
 		AccountDB accountDB = new AccountDB();
 
-    	accountDB.setAccountId(rs.getLong("id"));    	
+    	accountDB.setAccountId(rs.getLong("account.id"));    	
     	accountDB.setAccountName(rs.getString("name"));
     	accountDB.setAccountType(rs.getString("acType"));
     	
@@ -23,8 +25,8 @@ public class AccountRowMapper implements RowMapper<AccountDB>{
     	accountDB.setTimezone(rs.getString("timezone"));
     	accountDB.setStatus(rs.getString("status"));
     	
-    	accountDB.setCreatedDate(rs.getTimestamp("createdDate"));
-    	accountDB.setUpdatedDate(rs.getTimestamp("updatedDate"));
+    	accountDB.setCreatedDate(rs.getTimestamp("account.createdDate"));
+    	accountDB.setUpdatedDate(rs.getTimestamp("account.updatedDate"));
         
         accountDB.setPhoneNumber(rs.getString("phone"));
         accountDB.setGenre(rs.getString("genre"));
@@ -37,8 +39,23 @@ public class AccountRowMapper implements RowMapper<AccountDB>{
         
         accountDB.setImageUrl(rs.getString("logoPath"));		
         accountDB.setAdditionalDetails(rs.getString("additionalDetails"));
-        accountDB.setDeleted(rs.getInt("del"));
-		
+        accountDB.setDeleted(rs.getInt("account.del"));
+        
+        GeoTargetDB geoTargetDB = new GeoTargetDB();
+        geoTargetDB.setId(rs.getLong("geotarget.id"));
+        geoTargetDB.setAccountId(rs.getLong("adGroup_id"));
+        geoTargetDB.setAddress1(rs.getString("address1"));
+        geoTargetDB.setAddress2(rs.getString("address2"));
+        geoTargetDB.setCity(rs.getString("city"));
+        geoTargetDB.setState(rs.getString("state"));
+        geoTargetDB.setZipcode(rs.getLong("zipcode"));
+        
+        Location location = new Location();
+        location.setLatitude(rs.getString("lat"));
+        location.setLongitude(rs.getString("lng"));
+        geoTargetDB.setLocation(location);
+        
+        accountDB.setGeoTargetDB(geoTargetDB);
 		return accountDB;
 	}
 }
